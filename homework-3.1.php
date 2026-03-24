8fa6ede3336641d6dca9e8b0ed2435a727cc32aa
jinu0618-ui <jinuarins0618@gmail.com>
2026-03-24 13:26:05 +0900

    Update homework-3.1.php

--- homework-3.1.php
+++ homework-3.1.php
@@ -1,15 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+    숫자 입력: <input type="number" name="num">
+    <input type="submit" value="계산">
+</form>
+
 <?php
-$n = 30;
-$sum = 0;
-$prod = 1;
+if (isset($_POST['num'])) {
+    $n = $_POST['num'];
 
-for ($i = 1; $i <= $n; $i++) {
-    echo $i . " ";
-    $sum += $i;
-    $prod *= $i;
+    $sum = 0;
+    $prod = 1;
+
+    for ($i = 1; $i <= $n; $i++) {
+        echo $i . " ";
+        $sum += $i;
+        $prod *= $i;
+    }
+
+    echo "<br>";
+    echo "합: " . $sum . "<br>";
+    echo "곱(팩토리얼): " . $prod;
 }
-
-echo "<br>";
-echo "합: " . $sum . "<br>";
-echo "곱(팩토리얼): " . $prod;
 ?>
+
+</body>
+</html>
