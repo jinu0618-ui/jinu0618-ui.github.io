fa43c8b004178b307445a18a5f80797b428df535
jinu0618-ui <jinuarins0618@gmail.com>
2026-03-24 13:28:36 +0900

    Update homework-3.3.php

--- homework-3.3.php
+++ homework-3.3.php
@@ -1,21 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>피보나치 수열</title>
+</head>
+<body>
+
+<form method="post">
+    몇 번째 항까지 출력할지 입력:
+    <input type="number" name="num" min="2" required>
+    <input type="submit" value="확인">
+</form>
+
 <?php
-$n = 10;
+if (isset($_POST['num'])) {
+    $n = intval($_POST['num']);
 
-$f[1] = 1;
-$f[2] = 1;
-
-echo "i  f(i)  f(i+1)/f(i)<br>";
-
-for ($i = 3; $i <= $n; $i++) {
-    $f[$i] = $f[$i-1] + $f[$i-2];
-}
-
-for ($i = 1; $i <= $n; $i++) {
-    if ($i == 1 || $i == 2) {
-        echo "$i  {$f[$i]}  - <br>";
+    if ($n < 2) {
+        echo "2 이상의 숫자를 입력하세요.";
     } else {
-        $ratio = $f[$i] / $f[$i-1];
-        echo "$i  {$f[$i]}  " . number_format($ratio, 6) . "<br>";
+        $f[1] = 1;
+        $f[2] = 1;
+
+        echo "i &nbsp; f(i) &nbsp; f(i+1)/f(i)<br>";
+
+        for ($i = 3; $i <= $n; $i++) {
+            $f[$i] = $f[$i-1] + $f[$i-2];
+        }
+
+        for ($i = 1; $i <= $n; $i++) {
+            if ($i == 1 || $i == 2) {
+                echo "$i {$f[$i]} - <br>";
+            } else {
+                $ratio = $f[$i] / $f[$i-1];
+                echo "$i {$f[$i]} " . number_format($ratio, 6) . "<br>";
+            }
+        }
     }
 }
 ?>
+
+</body>
+</html>
