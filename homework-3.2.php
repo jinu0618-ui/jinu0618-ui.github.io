984e8a4cd171be4d50da8e32d7e540463e780451
jinu0618-ui <jinuarins0618@gmail.com>
2026-03-24 13:27:45 +0900

    Update homework-3.2.php

--- homework-3.2.php
+++ homework-3.2.php
@@ -1,23 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+    숫자들 입력 :<br>
+    <input type="text" name="nums" style="width:300px;">
+    <input type="submit" value="계산">
+</form>
+
 <?php
-$n = 30;
-$arr = [];
-$max = 0;
+if (isset($_POST['nums'])) {
 
-for ($i = 0; $i < $n; $i++) {
-    $arr[$i] = rand(1, 100);
-    echo $arr[$i] . " ";
+    $input = $_POST['nums'];
+    $arr = explode(" ", $input);
 
-    if ($max < $arr[$i]) {
-        $max = $arr[$i];
+   
+    $arr = array_map('intval', $arr);
+
+    $max = $arr[0];
+
+
+    foreach ($arr as $num) {
+        echo $num . " ";
+        if ($max < $num) {
+            $max = $num;
+        }
+    }
+
+    echo "<br>최댓값: " . $max . "<br>";
+
+    sort($arr);
+
+    echo "올림차순 정렬: ";
+    foreach ($arr as $num) {
+        echo $num . " ";
     }
 }
-
-echo "<br>최댓값: " . $max . "<br>";
-
-sort($arr);
-
-echo "올림차순 정렬: ";
-foreach ($arr as $num) {
-    echo $num . " ";
-}
 ?>
+
+</body>
+</html>
